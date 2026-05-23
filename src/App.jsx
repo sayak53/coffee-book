c06596aff9da346f0e157bfad9f9136f4c03c9d4
sayak53 <sayakchakraborty928@gmail.com>
2026-05-23 19:54:17 +0530

    added all files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-yellow-400">Coffee Book ☕</h1>
+    </div>
+  );
+}
+
+export default App;
