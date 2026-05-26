391f1f9e762947ccaed654678c1a64d579ee6799
sayak53 <sayakchakraborty928@gmail.com>
2026-05-26 19:43:06 +0530

    added all the coffee data and showed it dynamically

--- src/components/CoffeeCards.jsx
+++ src/components/CoffeeCards.jsx
@@ -1,14 +1,26 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import Card from "./Card";
+import { useEffect, useState } from "react";
 
 const CoffeeCards = () => {
   const data = useLoaderData();
   console.log(data);
   // eslint-disable-next-line no-unused-vars
   const { category } = useParams();
+  const [coffees, setCoffees] = useState([]);
+  useEffect(() => {
+    if (category) {
+      const filteredByCategory = [...data].filter(
+        (coffee) => coffee.category === category,
+      );
+      setCoffees(filteredByCategory);
+    } else {
+      setCoffees(data);
+    }
+  }, [category, data]);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      {data.map((coffee) => (
+      {coffees.map((coffee) => (
         <Card key={coffee.id} coffee={coffee} />
       ))}
     </div>
