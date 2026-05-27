316432d3b4eb8246bb21b5ecaabf734bc7e7765a
sayak53 <sayakchakraborty928@gmail.com>
2026-05-27 12:57:07 +0530

    added all the coffee cards to coffees section

--- src/pages/Coffees.jsx
+++ src/pages/Coffees.jsx
@@ -1,5 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+import Card from "../components/Card";
+
 const Coffees = () => {
-  return <div>coffees</div>;
+  const data = useLoaderData();
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12">
+      {data.map((coffee) => (
+        <Card key={coffee.id} coffee={coffee} />
+      ))}
+    </div>
+  );
 };
 
 export default Coffees;
