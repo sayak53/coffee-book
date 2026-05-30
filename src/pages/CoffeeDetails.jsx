c65ce1efbe9a47541ba87f0a37fd56772a550d4d
sayak53 <sayakchakraborty928@gmail.com>
2026-05-30 19:04:28 +0530

    a basic coffedetails page added

---
+++ src/pages/CoffeeDetails.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+
+const CoffeeDetails = () => {
+  const data = useLoaderData();
+  const { id } = useParams();
+  const [coffee, setCoffee] = useState({});
+  useEffect(() => {
+    const singleData = data.find((coffee) => coffee.id == id);
+    setCoffee(singleData);
+  }, [data, id]);
+  return <div>Coffee Deatils {coffee.description}</div>;
+};
+
+export default CoffeeDetails;
