b6884acf68496ab33b0b42f8e8536e9a4ef2cba3
sayak53 <sayakchakraborty928@gmail.com>
2026-05-31 19:13:59 +0530

    set items on LocalStorage

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 //get all coffees from local storage
 //add coffee to local storage
 const addFavourite = coffee => {
-    console.log(coffee)
+    const favourites = []
+    favourites.push(coffee)
+    localStorage.setItem('favourites',JSON.stringify(favourites))
 }
 //remove a coffee from local storage
 
