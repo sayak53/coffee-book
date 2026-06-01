0f133adbcf97457408246212a5cea77309c42b39
sayak53 <sayakchakraborty928@gmail.com>
2026-06-01 20:44:57 +0530

    new command added

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,8 @@
 //get all coffees from local storage
+
 //add coffee to local storage
 const addFavourite = coffee => {
+    //get all previously saved coffee data
     const favourites = []
     favourites.push(coffee)
     localStorage.setItem('favourites',JSON.stringify(favourites))
