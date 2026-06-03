626f580a450f180c63663c464abae838e6139678
sayak53 <sayakchakraborty928@gmail.com>
2026-06-03 13:43:34 +0530

    favourites coffees can be added to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import Heading from "../components/Heading";
+import { getAllFavourites } from "../utils";
+import Card from "../components/Card";
+
 const Dashboard = () => {
-  return <div>dashboard</div>;
+  const [coffees, setCoffees] = useState([]);
+  useEffect(() => {
+    const favourites = getAllFavourites();
+    setCoffees(favourites);
+  }, []);
+  return (
+    <>
+      <Heading
+        title="Welcome to Dashboard"
+        subtitle="Manage Coffes that you have previously added as favourite.You can view or remove them from here"
+      />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 my-12 w-[95%] mx-auto">
+        {coffees.map((coffee) => (
+          <Card key={coffee.id} coffee={coffee} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
